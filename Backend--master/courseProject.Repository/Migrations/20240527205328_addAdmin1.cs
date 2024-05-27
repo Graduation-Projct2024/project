f7007fc9d38c19b323ab95287cc2463028bb2f53
manar fuqha <m.a.fuqha1@students.ptuk.edu.ps>
2024-05-27 23:54:35 +0300

    edit admin

--- Backend--master/courseProject.Repository/Migrations/20240527205328_addAdmin1.cs
+++ Backend--master/courseProject.Repository/Migrations/20240527205328_addAdmin1.cs
@@ -5,19 +5,42 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace courseProject.Repository.Migrations
 {
-    public partial class addAdmin : Migration
+    public partial class addAdmin1 : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DeleteData(
                 table: "admins",
                 keyColumn: "AdminId",
-                keyValue: new Guid("d6f83557-8a8f-4cc8-847a-559035bb4401"));
+                keyValue: new Guid("e132c30a-3d4e-40c7-94d4-873d25afc56e"));
 
             migrationBuilder.DeleteData(
                 table: "users",
                 keyColumn: "UserId",
-                keyValue: new Guid("d6f83557-8a8f-4cc8-847a-559035bb4401"));
+                keyValue: new Guid("e132c30a-3d4e-40c7-94d4-873d25afc56e"));
+
+            migrationBuilder.InsertData(
+                table: "users",
+                columns: new[] { "UserId", "IsVerified", "email", "password", "role", "userName" },
+                values: new object[] { new Guid("23cab1b4-2f7a-49a8-a4fb-15d54f96ac88"), true, "programming.academy24@gmail.com", "$2a$11$cCCo7VY/4sxtIpeWDUFDROiDL/QPhi8AtawQhx4RGqKMAYgk8UIse", "admin", "admin" });
+
+            migrationBuilder.InsertData(
+                table: "admins",
+                columns: new[] { "AdminId", "DateOfBirth", "ImageUrl", "LName", "address", "gender", "phoneNumber" },
+                values: new object[] { new Guid("23cab1b4-2f7a-49a8-a4fb-15d54f96ac88"), null, null, null, null, null, null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "admins",
+                keyColumn: "AdminId",
+                keyValue: new Guid("23cab1b4-2f7a-49a8-a4fb-15d54f96ac88"));
+
+            migrationBuilder.DeleteData(
+                table: "users",
+                keyColumn: "UserId",
+                keyValue: new Guid("23cab1b4-2f7a-49a8-a4fb-15d54f96ac88"));
 
             migrationBuilder.InsertData(
                 table: "users",
@@ -28,29 +51,6 @@ namespace courseProject.Repository.Migrations
                 table: "admins",
                 columns: new[] { "AdminId", "DateOfBirth", "ImageUrl", "LName", "address", "gender", "phoneNumber" },
                 values: new object[] { new Guid("e132c30a-3d4e-40c7-94d4-873d25afc56e"), null, null, null, null, null, null });
-        }
-
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DeleteData(
-                table: "admins",
-                keyColumn: "AdminId",
-                keyValue: new Guid("e132c30a-3d4e-40c7-94d4-873d25afc56e"));
-
-            migrationBuilder.DeleteData(
-                table: "users",
-                keyColumn: "UserId",
-                keyValue: new Guid("e132c30a-3d4e-40c7-94d4-873d25afc56e"));
-
-            migrationBuilder.InsertData(
-                table: "users",
-                columns: new[] { "UserId", "IsVerified", "email", "password", "role", "userName" },
-                values: new object[] { new Guid("d6f83557-8a8f-4cc8-847a-559035bb4401"), false, "manar0fuqha2002189@gmail.com", "$2a$11$EmAYVIpLbbNEepKvCZovpODYakSaOGpTh9e7Osw2MdvDzEpjl.Y0W", "admin", "admin" });
-
-            migrationBuilder.InsertData(
-                table: "admins",
-                columns: new[] { "AdminId", "DateOfBirth", "ImageUrl", "LName", "address", "gender", "phoneNumber" },
-                values: new object[] { new Guid("d6f83557-8a8f-4cc8-847a-559035bb4401"), null, null, null, null, null, null });
         }
     }
 }
